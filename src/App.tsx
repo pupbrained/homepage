a7641329641376a68ef353f30acbd8279cc81d03
marsupialgutz <mars@possums.xyz>
2022-09-27 22:47:23 -0400

    i like this more

--- src/App.tsx
+++ src/App.tsx
@@ -94,14 +94,14 @@ export default function App() {
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
         transition={{ duration: 0.4 }}
-        className='h-[500px] w-[800px] bg-ctp-base rounded-2xl flex'
+        className='h-[500px] w-[800px] bg-ctp-base rounded-2xl flex select-none'
       >
         <div
           className="bg-[url('/src/assets/imgs/sideimg.gif')] bg-cover bg-center bg-no-repeat w-[400px] rounded-l-2xl after:content-[''] after:block after:ml-[248px] after:bg-ctp-base after:w-4 after:h-[500px] after:mt-[-500px] after:rounded-l-2xl"
           title='A city during the sunset'
         >
           <div className='bg-[#1e1e2eaa] h-[276px] my-28 ml-16 mr-[80px] rounded-2xl text-7xl text-center flex items-center backdrop-blur-sm text-ctp-subtext1'>
-            <div className='flex flex-col w-full select-none'>
+            <div className='flex flex-col w-full'>
               <span>{moment().format('hh')}</span>
               <span>-</span>
               <span>{moment().format('mm')}</span>
